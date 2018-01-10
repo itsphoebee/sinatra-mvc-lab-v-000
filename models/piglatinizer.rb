bac318232ca1005a541765520d46762ad0735a7c
Phoebe Lau <phoebelau7@gmail.com>
2018-01-10 17:25:03 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,7 +2,7 @@ class PigLatinizer
 
   def to_pig_latin(sentence)
     new_text = sentence.split(" ").map do |word|
-      word.to_piglatinize
+      to_piglatinize(word)
     end
     new_text.join(" ")
   end
