571dd52c727e2036e19add94d0cfd930e52873d0
Phoebe Lau <phoebelau7@gmail.com>
2018-01-10 16:55:22 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -13,8 +13,7 @@ class PigLatinizer
     @new.map do |word|
       if word.start_with?('a','e','i','o','u')
         word << 'ay'
-      else word.split('')
-        if word[1].include?('a'||'e'||'i'||'o'||'u')
+      elsif word[1].include?('a'||'e'||'i'||'o'||'u')
           word << word.slice(0)+'ay'
           word[0] = ''
           word
