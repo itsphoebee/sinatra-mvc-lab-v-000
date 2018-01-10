27d5415b6aff8600ec3e31a196abf498486595cf
Phoebe Lau <phoebelau7@gmail.com>
2018-01-10 16:42:20 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -12,9 +12,13 @@ class PigLatinize
         word << 'ay'
       else word.split('')
         if word[1].include?('a'||'e'||'i'||'o'||'u')
-        word << word.slice(0)+'ay'
-        word[0] = ''
-        word
+          word << word.slice(0)+'ay'
+          word[0] = ''
+          word
+        else 
+          word << word.slice(0..1)+'ay'
+          word[0..1] = ''
+          word
       end
     end
   end
