cd08635645a49ed621f47ab6c1408fed60ee2a9f
Phoebe Lau <phoebelau7@gmail.com>
2018-01-10 16:55:37 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -14,13 +14,13 @@ class PigLatinizer
       if word.start_with?('a','e','i','o','u')
         word << 'ay'
       elsif word[1].include?('a'||'e'||'i'||'o'||'u')
-          word << word.slice(0)+'ay'
-          word[0] = ''
-          word
-        else
-          word << word.slice(0..1)+'ay'
-          word[0..1] = ''
-          word
+        word << word.slice(0)+'ay'
+        word[0] = ''
+        word
+      else
+        word << word.slice(0..1)+'ay'
+        word[0..1] = ''
+        word
         end
       end
     end
