f609fadcd7a18a824259b95e92d30dab6b7b0e41
Phoebe Lau <phoebelau7@gmail.com>
2018-01-10 16:28:06 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,5 +6,15 @@ class PigLatinize
   end
 
   def to_piglatinize
+    new_text = @text.split(' ')
+    new_text.map do |word|
+      if word.start_with?('a','e','i','o','u')
+        word << 'ay'
+      elsif 
+        word << word.slice(0)+'ay'
+        word[0] = ''
+        word
+      end
+    end
   end
 end
